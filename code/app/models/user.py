0cd0ae6e651dbf7fc9898fb3f6af3a0c7e762e9d
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2025-01-17 19:39:42 +0200

    done hw

--- code/app/models/user.py
+++ code/app/models/user.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 
 class User(db.Model):
-    __tablename__ = "user" 
+    __tablename__ = "user"  # Це на дз
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
     surname = db.Column(db.String(50))
     email = db.Column(db.String(320), unique=True)
     created_at = db.Column(db.Date, default=datetime.now())
 
-    posts = db.relationship("Post", back_populates="user", cascade="all")
+    posts = db.relationship("post", back_populates="user", cascade="all")
